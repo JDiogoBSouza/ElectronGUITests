f47f74ac3a9219da182f5c6ca6faba652e75f989
Diogo Souza <di0g0-dg@Hotmail.com>
2021-03-05 17:20:58 -0300

    Alterações na UI e adição do UDP Server.

--- app/js/rendererMainWindow.js
+++ app/js/rendererMainWindow.js
@@ -1,6 +1,12 @@
 const {ipcRenderer} = require('electron');
 const fs = require('fs')
 
+// UDP Server imports
+const dgram = require('dgram');
+// Performance UDP Solution
+const Dequeue = require('dequeue');
+const FIFO = new Dequeue();
+
 // Configuration file path
 const pelvConfig_path = './pelv.conf'
 
@@ -8,48 +14,56 @@ const pelvConfig_path = './pelv.conf'
 const btnRealTime = document.getElementById("btn_realTime");
 const btnFtp = document.getElementById("btn_ftp");
 
+// Actual mode
+var pelvwareMode = 0;           // 0 - Realtime, 1 - FTP
+
 btnRealTime.onclick = function()
 {
-    btnFtp.classList.remove("active");
-    btnFtp.disabled = false;
-    btnRealTime.classList.add("active");
-    btnRealTime.disabled = true;
+    // TODO: Limpar o gráfico atual para a plotagem do gráfico do Realtime.
 
-    // Active RealTime controller buttons
-    btnPlay.classList.remove("active");
-    btnPlay.disabled = false;
-    btnPause.classList.remove("active");
-    btnPause.disabled = false;
-    btnStop.classList.remove("active");
-    btnStop.disabled = false;
-    btnSave.classList.remove("active");
-    btnSave.disabled = false;
+    if( pelvwareMode != 0 ){
 
-    // Deactive FTP Open file button
-    btnOpenFile.classList.add("active");
-    btnOpenFile.disabled = true;
+        pelvwareMode = 0;
+
+        btnFtp.classList.remove("active");
+        btnFtp.disabled = false;
+        btnRealTime.classList.add("active");
+        btnRealTime.disabled = true;
+    
+        // Active RealTime controller buttons
+        btnPlay.classList.remove("active");
+        btnPlay.disabled = false;
+    
+        // Deactive FTP Open file button
+        btnOpenFile.classList.add("active");
+        btnOpenFile.disabled = true;
+    }
+    
 };
 
 btnFtp.onclick = function()
 {
-    btnRealTime.classList.remove("active");
-    btnRealTime.disabled = false;
-    btnFtp.classList.add("active");
-    btnFtp.disabled = true;
+    // TODO: Verificar se deseja salvar os dados do gráfico ao mudar do modo realtime para FTP.
+    
+    // TODO: Limpar o gráfico atual para a plotagem do gráfico do FTP.
+    if( pelvwareMode != 1 ){
 
-    // Deactive RealTime controller buttons
-    btnPlay.classList.add("active");
-    btnPlay.disabled = true;
-    btnPause.classList.add("active");
-    btnPause.disabled = true;
-    btnStop.classList.add("active");
-    btnStop.disabled = true;
-    btnSave.classList.add("active");
-    btnSave.disabled = true;
+        pelvwareMode = 1;
 
-    // Active FTP Open file button
-    btnOpenFile.classList.remove("active");
-    btnOpenFile.disabled = false;
+        btnRealTime.classList.remove("active");
+        btnRealTime.disabled = false;
+        btnFtp.classList.add("active");
+        btnFtp.disabled = true;
+    
+        // Deactive RealTime controller buttons
+        btnPlay.classList.add("active");
+        btnPlay.disabled = true;
+    
+        // Active FTP Open file button
+        btnOpenFile.classList.remove("active");
+        btnOpenFile.disabled = false;
+    }
+    
 };
 
 // System buttons
@@ -147,8 +161,9 @@ anychart.onDocumentReady(function()
     chart.xAxis().title("Tempo (ms)");
     chart.yAxis().title("Valor (mv)");
 
+    // TODO: Set auto ajust in Y axis.
     chart.yScale().minimum(0);
-    chart.yScale().maximum(50);
+    chart.yScale().maximum(2);
 
     // add a listener
     chart.xScroller().listen("scrollerchangefinish", function(e){
@@ -174,3 +189,104 @@ btnConfigure.addEventListener('click', function(e) {
     dataSet.append({"time": contador, "value": valor});
     contador++;*/
 });
+
+// Receive data from udp here
+var server = dgram.createSocket('udp4');
+
+// Starts manually udp server.
+btnPlay.addEventListener('click', function(e) {
+
+    console.log("Iniciando Recepção...");
+
+    // Disable Play and Save buttons
+    btnPlay.classList.add("active");
+    btnPlay.disabled = true;
+    btnSave.classList.add("active");
+    btnSave.disabled = true;
+
+    // Enable Pause and Stop buttons
+    btnPause.classList.remove("active");
+    btnPause.disabled = false;
+    btnStop.classList.remove("active");
+    btnStop.disabled = false;
+
+    // Start UDP Server and ?? Send Command to Request Data to ESP ??
+    try{
+        server.bind(5000, () => {
+            // Do Something after bind ?
+        });
+    }
+    catch(err){
+        console.log("Bind Error: " + err.message);
+    }
+});
+
+server.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+    //console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+
+    // Separe value and timestamp.
+    let data = (msg+'').split(';');
+
+    // TODO: Check if data is complete
+    if( data.length == 2){
+
+        //console.log("Dado Original: " + msg)
+        //console.log(`Timestamp: ${data[0]} Data: ${data[1]}`);
+        
+        // Put data in a FIFO
+        FIFO.push(data);
+    }
+    else{
+        console.log("Dado imcompleto: " + msg)
+    }
+
+});
+
+server.on('listening', () => {
+    const address = server.address();
+    console.log(`server listening ${address.address}:${address.port}`);
+});
+
+// TODO: It's not working for now. Make changes.
+btnStop.addEventListener('click', function(e) {
+    
+    // Enable Play and Save buttons
+    btnPlay.classList.remove("active");
+    btnPlay.disabled = false;
+    btnSave.classList.remove("active");
+    btnSave.disabled = false;
+
+    // Disable Pause and Stop buttons
+    btnPause.classList.add("active");
+    btnPause.disabled = true;
+    btnStop.classList.add("active");
+    btnStop.disabled = true;
+
+    // Stop UDP Server and ?? Send Command to ESP Stop ??
+    server.close( () => {
+        // Do Something afet close ?
+    });
+});
+
+// Start UDP Server
+fetcher();
+
+let start1 = 0;
+
+function fetcher() {
+
+    if (FIFO.length > 0){
+        var dataFIFO = FIFO.shift();
+
+        // Put graph here.
+        dataSet.append({"time": dataFIFO[0], "value": dataFIFO[1]});
+    }
+
+    setImmediate(fetcher); //make this function continuously run
+}
+
